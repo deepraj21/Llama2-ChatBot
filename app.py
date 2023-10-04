889976a0ee8a52fb9cb4a1518de0c0dc3bf81cc8
Deepraj <83288891+deepraj21@users.noreply.github.com>
2023-10-04 22:45:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import replicate
+
+st.set_page_config(page_title="Llama 2 Chatbot",page_icon="🦙")
+
+replicate_api = st.secrets['REPLICATE_API_TOKEN']
+with st.sidebar:
+    st.title('🦙 Llama 2 Chatbot')
+    
+    st.subheader('Models and parameters')
+    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
+    if selected_model == 'Llama2-7B':
+        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
+    elif selected_model == 'Llama2-13B':
+        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
+    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+    max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
+    st.markdown('📖 Checkout my [Github](https://github.com/deepraj21)!')
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    # string_dialogue = "You are a medical assistant.You can suggest medicine to the given symptoms"
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+    return output
+
+if prompt := st.chat_input(disabled=not replicate_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
---
+++ readme.md
@@ -0,0 +1,32 @@
+
+## Llama 2 Chatbot with Streamlit and Replicate
+
+### Overview
+
+This code implements a chatbot using the Llama 2 language model, powered by the Streamlit framework and Replicate API. The chatbot engages in conversations with users, responding to prompts in a natural language manner.
+
+### Features
+
+- **Model Selection:** Users can choose between different Llama 2 models, such as 'Llama2-7B' and 'Llama2-13B', each with its unique capabilities.
+
+- **Parameter Adjustment:** The chatbot's behavior can be fine-tuned with parameters like temperature, top_p, and max_length, providing flexibility in generating responses.
+
+- **Chat History Management:** Users can clear the chat history with a button, facilitating a clean conversation interface.
+
+### Getting Started
+
+1. **API Token:** Ensure that you have a Replicate API token, and set it in the Streamlit secrets for secure access.
+
+2. **Model Selection:** Choose a Llama 2 model and set parameters in the sidebar.
+
+3. **Interact:** Users can input prompts using the chat interface, and the chatbot generates responses dynamically.
+
+### Dependencies
+
+- [Streamlit](https://streamlit.io/)
+- [Replicate API](https://replicate.ai/)
+
+### WebApp Preview
+
+<img src="template.png">
+
